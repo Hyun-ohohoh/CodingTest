0140412e7a5263e921f0e7fa97beaad7596b4050
NAHO <ohmap@naver.com>
2026-03-04 13:21:19 +0900

    Solve - BOJ 1026

---
+++ src/S23greedy/Q1026_260304.java
@@ -0,0 +1,42 @@
+package S23greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Q1026_260304 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Integer[] a = new Integer[n];
+        Integer[] b = new Integer[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+        Arrays.sort(b, Comparator.reverseOrder());
+
+        int result = 0;
+        for(int i = 0; i < n; i++) {
+            result += a[i] * b[i];
+        }
+
+        System.out.println(result);
+
+
+    }
+}
