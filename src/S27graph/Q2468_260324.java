e2230b698031dc6db4feb3a5d1d9ed31a759fdb4
NAHO <ohmap@naver.com>
2026-03-24 15:45:02 +0900

    Attempt: BOJ 2468(DFS, StackOverflow)

---
+++ src/S27graph/Q2468_260324.java
@@ -0,0 +1,100 @@
+package S27graph;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * n: 2이상 100 이하의 정수 -> 최데 10000칸
+ * 모든 칸이 잠기지 않았다면 DFS 재귀 10000번
+ * -> StackOverFlow(자바의 스택 메모리 크기는 보통 1MB 내외)
+ * BFS로 푸는 방식을 고려해봐야함
+ */
+public class Q2468_260324 {
+
+    static int n;
+    static int[][] arr;
+    static int[][] drown;
+    static int[][] visited;
+
+    static int[] dRow = {0, 0, 1, -1};
+    static int[] dCol = {1, -1, 0, 0};
+
+    static void rain(int height) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(arr[i][j] <= height) {
+                    drown[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    static void search(int row, int col, int height) {
+        visited[row][col] = 1;
+
+        for(int i = 0; i < 4; i++) {
+            int nextRow = row + dRow[i];
+            int nextCol = col + dCol[i];
+
+            if(nextRow < 0 || nextCol < 0 || nextRow >= n || nextCol >= n) {
+                continue;
+            }
+
+            if(drown[nextRow][nextCol] == 0 && visited[nextRow][nextCol] == 0) {
+                search(nextRow, nextCol, height);
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n][n];
+        drown = new int[n][n];
+        visited = new int[n][n];
+
+
+        int maxHeight = 0;
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if(maxHeight < arr[i][j]) {
+                    maxHeight = arr[i][j];
+                }
+            }
+        }
+
+
+        int maxCount = 0;
+
+        for(int height = 0; height <= maxHeight; height++){
+            int count = 0;
+            rain(height);
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(drown[i][j] == 0 && visited[i][j] == 0) {
+                        search(i, j, height);
+                        count++;
+                    }
+                }
+            }
+            maxCount = Math.max(count, maxCount);
+            visited = new int[n][n];
+            drown = new int[n][n];
+        }
+
+
+        System.out.println(maxCount);
+
+
+    }
+}
