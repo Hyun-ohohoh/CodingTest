d3919aa3320fa1cd7d7e7940a38b3286c2831a67
NAHO <ohmap@naver.com>
2026-04-01 16:49:26 +0900

    Attempt: BOJ 11726(Integer Overflow)

---
+++ src/S21dp/Q11726_260401.java
@@ -0,0 +1,27 @@
+package S21dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q11726_260401 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int width = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[width + 1];
+        dp[1] = 1;
+        if(width != 1) {
+            dp[2] = 2;
+        }
+
+        for(int i = 3; i <= width; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        System.out.println(dp[width] % 10007);
+    }
+}
