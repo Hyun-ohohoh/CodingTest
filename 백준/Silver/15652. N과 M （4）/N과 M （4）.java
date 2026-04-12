277e56fff09dec980b41046e4da34961890960f3
NAHO <ohmap@naver.com>
2026-04-12 15:17:13 +0900

    [Silver III] Title: N과 M (4), Time: 128 ms, Memory: 17292 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    static int[] output;
+
+    static StringBuilder sb;
+
+    static void combinationWithRepetition(int depth, int start, int n, int m) {
+        if(depth == m) {
+            for(int i = 0; i < m; i++) {
+                sb.append(output[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i < n; i++) {
+            output[depth] = arr[i];
+            combinationWithRepetition(depth+1, i, n, m);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        output = new int[m];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
+
+        combinationWithRepetition(0,0, n, m);
+
+        System.out.println(sb);
+
+
+    }
+}
