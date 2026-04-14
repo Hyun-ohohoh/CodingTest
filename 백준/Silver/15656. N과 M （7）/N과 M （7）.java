350f0623cfbeee5d473c6cc38c8fc05a39ae3330
NAHO <ohmap@naver.com>
2026-04-14 11:46:42 +0900

    [Silver III] Title: N과 M (7), Time: 560 ms, Memory: 123160 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    static int[] output;
+
+    static StringBuilder sb;
+
+    static void permutationWithRepetition(int depth, int n, int m) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(output[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            output[depth] = arr[i];
+            permutationWithRepetition(depth + 1, n, m);
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        arr = new int[n];
+        output = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        permutationWithRepetition(0, n, m);
+
+        System.out.println(sb);
+
+
+    }
+}
