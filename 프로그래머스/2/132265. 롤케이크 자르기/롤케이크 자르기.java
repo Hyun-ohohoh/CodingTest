380d2715c92cf51df8191bd4d50db0b5ffd99f95
NAHO <ohmap@naver.com>
2026-07-01 15:15:21 +0900

    [level 2] Title: 롤케이크 자르기, Time: 136.33 ms, Memory: 238 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -3,28 +3,29 @@ import java.util.*;
 class Solution {
    public int solution(int[] topping) {
 
+        Map<Integer, Integer> chulsoo = new HashMap<>();
+        Set<Integer> brother = new HashSet<>();
         int result = 0;
         
-        // Key: 토핑 종류, Value: 토핑 개수
-        Map<Integer, Integer> brotherMap = new HashMap<>();
-        for (int t : topping) {
-            if (brotherMap.containsKey(t)) {
-                brotherMap.put(t, brotherMap.get(t) + 1);
+        for(int i = 0; i < topping.length; i++) {
+            int value = topping[i];
+            if(!chulsoo.containsKey(value)) {
+                chulsoo.put(value, 1);
             } else {
-                brotherMap.put(t, 1);
+                chulsoo.put(value, chulsoo.get(value) + 1);
             }
         }
 
-        Set<Integer> chulsooSet = new HashSet<>();
         for(int i = 0; i < topping.length; i++) {
             int value = topping[i];
-            chulsooSet.add(value);
-            brotherMap.put(value, brotherMap.get(value) - 1);
-            if(brotherMap.get(value) == 0) {
-                brotherMap.remove(value);
+            brother.add(value);
+            
+            chulsoo.put(value, chulsoo.get(value) - 1);
+            if(chulsoo.get(value) == 0) {
+                chulsoo.remove(value);
             }
-
-            if(chulsooSet.size() == brotherMap.size()) {
+            
+            if(chulsoo.size() == brother.size()) {
                 result += 1;
             }
         }
