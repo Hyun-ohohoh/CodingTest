07bf40693e407ae624c826fd6185d82c00485a9a
NAHO <ohmap@naver.com>
2026-07-11 19:12:32 +0900

    [level 2] Title: 피로도, Time: 2.28 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -5,12 +5,12 @@ class Solution {
         return search(k, dungeons, visited, 0);
     }
 
-    int search(int k, int[][] dungeons, boolean[] visited, int count) {
+    int search(int point, int[][] dungeons, boolean[] visited, int count) {
         int max = count;
         for(int i = 0; i < dungeons.length; i++) {
-            if(k >= dungeons[i][0] && !visited[i]) {
+            if(point >= dungeons[i][0] && !visited[i] ) {
                 visited[i] = true;
-                max = Math.max(max, search(k-dungeons[i][1], dungeons, visited, count+1));
+                max = Math.max(max, search(point - dungeons[i][1], dungeons, visited, count+1));
                 visited[i] = false;
             }
         }
