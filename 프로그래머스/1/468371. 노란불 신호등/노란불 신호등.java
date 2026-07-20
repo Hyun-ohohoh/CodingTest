6e6ce90dc3417952063cb6ea29721c97f3516380
NAHO <ohmap@naver.com>
2026-07-20 18:06:30 +0900

    [level 1] Title: 노란불 신호등, Time: 10.40 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int[][] signals) {
         int signalCount = signals.length;
-        
+
         int iteration = 1;
         for(int[] signal : signals) {
-            int sum = signal[0] + signal[1] + signal[2];
-            iteration *= sum;
+            int length = signal[0] + signal[1] + signal[2];
+            iteration = lcm(iteration, length);
         }
 
         int t = 1;
@@ -21,11 +21,23 @@ class Solution {
             if(allYellow) {
                 return t;
             }
-            
+
             t += 1;
         }
-        
+
         return -1;
+    }
+    
+    // 최대공약수
+    int gcd(int a, int b) {
+        if(b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    // 최소공배수
+    int lcm(int a, int b) {
+        if(a == 0 || b == 0) return 0;
+        return a / gcd(a, b) * b;
     }
 
     boolean isYellow(int[] signal, int t) {
